ff1b8d8e5d8ee4c3356344684166e800137113a7
DaGri <david.griegel.1991@gmail.com>
2016-05-20 17:46:45 +0200

    Update for presentation
    Alles so weit drin!

--- app/src/main/java/com/example/dagri/googlemapsapi/MapActivity.java
+++ app/src/main/java/com/example/dagri/googlemapsapi/MapActivity.java
@@ -15,10 +15,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Polygon;
@@ -28,13 +29,18 @@ import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
+    // GOOGLE MAP OBJEKT ERSTELLEN, DAS IM WEITEREN VERLAUF VERWENDET WIRD
     private GoogleMap mMap;
+
+    // GOOGLE API CLIENT OBJEKT ERSTELLEN, DAS IM WEITEREN VERLAUF VERWENDET WIRD
     GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // GOOGLE API CLIENT BAUEN
+        // TODO : WOFUER WIRD DER GOOGLE API CLIENT BENOETIGT?
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */,
                         this /* OnConnectionFailedListener */)
@@ -49,56 +55,29 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         mapFragment.getMapAsync(this);
     }
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        // MAP OBJEKT ERSTELLEN
         mMap = googleMap;
 
+        // BERECHTIGUNGEN UEBERPRUEFEN FUER DIE GENAUE POSITION
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
+            // WENN BERECHTIGUNG VORHANDEN, DANN DEN BUTTON EINSCHALTEN
             mMap.setMyLocationEnabled(true);
         } else {
-            // Show rationale and request permission.
+            // TODO : NACHFRAGE AN DEN NUTZER STELLEN
         }
 
-
+        // EINE POSITION IN LATITUDE UND LONGITUDE SPEICHERN
         double lat = 51.4477;
         double lon = 7.2711;
+
+        // LATLNG-ONJEKT ERSTELLEN UND HOCHSCHULE BENENNEN
         LatLng hochschule = new LatLng(lat, lon);
+
+        // KAMERA (ANSICHT) AUF DIE STARTPOSITION AUSRICHTEN
         mMap.moveCamera(CameraUpdateFactory.newLatLng(hochschule));
-
-        // https://developers.google.com/maps/documentation/android-api/shapes#polygone
-
-        // Instantiates a new Polyline object and adds points to define a rectangle
-        PolylineOptions rectOptionsPolyline = new PolylineOptions()
-                .add(new LatLng(-33.35, 152.0))
-                .add(new LatLng(-33.45, 152.0)) // North of the previous point, but at the same longitude
-                .add(new LatLng(-33.45, 152.2)) // Same latitude, and 30km to the west
-                .add(new LatLng(-33.35, 152.2)) // Same longitude, and 16km to the south
-                .color(Color.CYAN);
-
-// Get back the mutable Polyline
-        Polyline polyline = mMap.addPolyline(rectOptionsPolyline);
-
-        // Instantiates a new Polygon object and adds points to define a rectangle
-        PolygonOptions rectOptionsPolygon = new PolygonOptions()
-                .add(new LatLng(-33.35, 153.0), new LatLng(-33.45, 153.0),
-                        new LatLng(-33.45, 153.2),
-                        new LatLng(-33.35, 153.2),
-                        // DIE LETZTE KOORDINATE MUSS NICHT GLEICH DER ERSTEN SEIN
-                        new LatLng(-33.35, 153.0));
-
-// Get back the mutable Polygon
-        Polygon polygon = mMap.addPolygon(rectOptionsPolygon);
-
-        // Instantiates a new CircleOptions object and defines the center and radius
-        CircleOptions circleOptions = new CircleOptions()
-                .center(new LatLng(-34.4, 155.1))
-                .radius(1000); // In meters
-
-// Get back the mutable Circle
-        Circle circle = mMap.addCircle(circleOptions);
-
 
         // KARTENTYPEN SETZTEN:
         // mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -115,31 +94,81 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         mMap.getUiSettings().setIndoorLevelPickerEnabled(true);
         // MAP TOOLBAR EINSCHALTEN
         mMap.getUiSettings().setMapToolbarEnabled(true);
-        // MEINE POSITION BUTTON EINSCHALTEN
-        // https://developers.google.com/maps/documentation/android-api/location#laufzeitberechtigungen_anfordern
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
+        // KREIS ERSTELLEN
+        CircleOptions circleOptions = new CircleOptions()
+                .center(new LatLng(51.77, 7.33)) // KOORDINATE DES ZENTRUMS
+                .radius(500) // RADIUS IN METERN
+                .fillColor(Color.BLUE); // FUELLFARBE
+
+        // KREIS ZUR KARTE HINZUFUEGEN
+        Circle circle = mMap.addCircle(circleOptions);
+
+        // Instantiates a new Polyline object and adds points to define a rectangle
+        PolylineOptions rectOptionsPolyline = new PolylineOptions()
+                .add(new LatLng(-33.35, 152.0))
+                .add(new LatLng(-33.45, 152.0)) // North of the previous point, but at the same longitude
+                .add(new LatLng(-33.45, 152.2)) // Same latitude, and 30km to the west
+                .add(new LatLng(-33.35, 152.2)) // Same longitude, and 16km to the south
+                .color(Color.CYAN);
+
+        // Get back the mutable Polyline
+        Polyline polyline = mMap.addPolyline(rectOptionsPolyline);
+
+        // Instantiates a new Polygon object and adds points to define a rectangle
+        PolygonOptions rectOptionsPolygon = new PolygonOptions()
+                .add(new LatLng(-33.35, 153.0), new LatLng(-33.45, 153.0),
+                        new LatLng(-33.45, 153.2),
+                        new LatLng(-33.35, 153.2),
+                        // DIE LETZTE KOORDINATE MUSS NICHT GLEICH DER ERSTEN SEIN
+                        new LatLng(-33.35, 153.0));
+
+        // Get back the mutable Polygon
+        Polygon polygon = mMap.addPolygon(rectOptionsPolygon);
+
+
+
+
+
+        // MARKER KREISFOERMIG ERSTELLEN
+        // FARBEN UND TRANSPARENZ WERDEN ZUFAELLIG ERSTELLEN
         double radius = 0.02;
-
 
         for (double a = 0; a < 2 * Math.PI; a = a + 0.1) {
             double actLon = lon + Math.cos(a) * radius;
             double actLat = lat + Math.sin(a) * radius;
 
             mMap.addMarker(new MarkerOptions()
-                    .position(
-                            new LatLng(actLat, actLon)
-
-                    )
+                    // POSITION SETZTEN
+                    .position(new LatLng(actLat, actLon))
+                    // FARBE SETZTEN
                     .icon(BitmapDescriptorFactory.defaultMarker((float) Math.random() * 255))
-                    .title("Sinnloser Punkt in der Landschaft!")
+                    // TITEL BEIM ANKLICKEN SETZTEN
+                    .title("Nutzloser Punkt in der Landschaft!")
+                    // TITEL UNTERSCHRIFT SETZTEN
                     .snippet("Dies ist zusaetzlicher nutzloser Text unter dem Titel!")
+                    // ALPHA WERT DES PUNKTES SETZTEN
                     .alpha((float) Math.random())
+                    // PUNKT VERSCHIEBBAR SCHALTEN
                     .draggable(true)
+                    // MARKER FLAT
+                    // .flat(true)
             );
         }
 
-        // TODO : UEBERLAGERUNGEN
+        // UEBERLAGERUNG ERSTELLEN
+        // KOORDINATE ERSTELLEN
+        LatLngBounds ueberlagerungKoordinate = new LatLngBounds(
+                new LatLng(51.8, 7.5),       // SUED WESTLICHE ECKE
+                new LatLng(52.0, 8.8));      // NORD OESTLICHE ECKE
+        // GROUND OVERLAY OBJEKT ERSTELLEN
+        GroundOverlayOptions ueberlagerung = new GroundOverlayOptions()
+                // BILD-DATEI VERLINKEN
+                .image(BitmapDescriptorFactory.fromResource(R.mipmap.androidlogo))
+                // POSITION ZUWEISEN
+                .positionFromBounds(ueberlagerungKoordinate);
+        // UEBERLAGERUNG ZUR KARTE HINZUFUEGEN
+        mMap.addGroundOverlay(ueberlagerung);
 
     }
 
@@ -151,11 +180,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     }
 
     protected void onStart() {
+        // VERBINDEN MIT DEM GOOGLE API CLIENT
         mGoogleApiClient.connect();
         super.onStart();
     }
 
     protected void onStop() {
+        // TRENNEN VON DEM GOOGLE API CLIENT
+        // SPART STROM
         mGoogleApiClient.disconnect();
         super.onStop();
     }
